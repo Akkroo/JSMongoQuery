5dd613c9c0c40c5b66ebd79c6dfdceab3dcfe2df
Austin Birch <mraustinbirch@gmail.com>
2013-07-20 20:37:18 +0100

    First pass at fixing relative links for html test runner
    Currently you must run the tests by running a server from the project root
    (for example using 'python -m SimpleHTTPServer'), and then navigating to
    localhost:8000/spec/runner.html.

--- spec/requirejsconf.js
+++ spec/requirejsconf.js
@@ -1,16 +1,15 @@
-/* 
+/*
  * Copyright (C) 2013 Akkroo Solutions Ltd
- * 
+ *
  */
 
 requirejs.config({
 	paths: {
-		"app":"../../app/resources/js",
-		"domReady": "../../app/resources/js/libs/require/domReady",
-		"text": "../../app/resources/js/libs/require/text",
-		"underscore": "../../app/resources/js/libs/underscore",
-		"jasmine": "jasmine-1.3.1/jasmine",
-		"jasmine-html": "jasmine-1.3.1/jasmine-html"
+		"domReady": "./vendor/require/domReady",
+		"text": "./vendor/require/text",
+		"underscore": "./vendor/underscore",
+		"jasmine": "./vendor/jasmine-1.3.1/jasmine",
+		"jasmine-html": "./vendor/jasmine-1.3.1/jasmine-html"
 	},
 	shim: {
 		"underscore": {
