ab2ece7f484ab5998fb1c083c905b83f93e1a0b2
Nick Brook <nick@rmpenterprise.co.uk>
2013-07-28 18:05:52 +0100

    All tests passing - fix for searching inside arrays
    Signed-off-by: Nick Brook <nick@rmpenterprise.co.uk>

--- spec/JSMongoQuery.spec.js
+++ spec/JSMongoQuery.spec.js
@@ -10,10 +10,19 @@
 	if (!typeof queries === 'object') {
 		queries = JSON.parse(testQueries);
 	}
+	
+	var logger = {
+		debug: function(e,p) { console.log(e); console.log(p); },
+		warning: function(e,p) { console.warn(e); console.warn(p); },
+		error: function(e,p) { console.error(e); console.error(p); }
+	};
 
 	describe("The JSMongoQuery performs query", function() {
 		queries.forEach(function(q) {
 			it(q.description, function() {
+				// uncomment for debugging
+//				q.options.logger = logger;
+//				q.options.debug = true;
 				expect(JSMongoQuery.executeQuery(q.query, q.document, q.options)).toBe(q.result);
 			});
 		});
